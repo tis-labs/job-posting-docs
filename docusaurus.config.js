20f25dab1ad915a2eb924e24e0a3f59350e1c20d
this-is-spear <92219795+this-is-spear@users.noreply.github.com>
2025-01-17 23:36:13 +0900

    chore : java 언어 하이라이트 추가

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -142,6 +142,7 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
+        additionalLanguages: ['java'],
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
